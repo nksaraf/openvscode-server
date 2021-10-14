cbd0c80cf2c564710cd9dc73ad955f1fc8a8a5cc
Nikhil Saraf <nsaraf@purestorage.com>
2021-10-15 04:21:50 +0530

    performance package esm

--- src/vs/base/common/performance.ts
+++ src/vs/base/common/performance.ts
@@ -3,14 +3,16 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+export const mark = (...args: Parameters<typeof performance.mark>) =>
+	performance.mark(...args);
+
 export interface PerformanceMark {
 	readonly name: string;
 	readonly startTime: number;
 }
 
-export function mark(name: string): void;
 
 /**
  * Returns all marks, sorted by `startTime`.
  */
-export function getMarks(): PerformanceMark[];
+export const getMarks = performance.getEntries;
